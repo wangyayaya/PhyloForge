e7ff825a2028023f9e3e96945cb9d407bb5e8a12
wangyayaya <1552082076@qq.com>
2023-06-17 10:58:11 +0800

    update

--- run.py
+++ run.py
@@ -1,17 +1,7 @@
-import argparse
-import os
-import sys
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
-
-from script import script,hmm2OG,mode
-cmd = script.RunCmd()
-
+from script import mode
 
 if __name__ == '__main__':
-    #cmd.mkdir()
-    #script.format_and_trans_mul()
-    #cmd.run_hmmscan_pl()
-    #hmm2OG.HMM_OG().run_hmm2OG()
-    #cmd.run_tree_mul()
-    #cmd.get_super_gene()
     mode.MODE().mode0()
